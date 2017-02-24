24a0d24db6396c815f480e4cd90f3901b0c05d0e
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-24 08:21:38 -0500

    updating model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,29 +2,47 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def edit
-    @post = Post.find(params[:id])
-  end
+      @post = Post.find(params[:id])
+      if @post.user == current_user
+        @post.destroy
+      else
+        flash[:alert] = "Only the author of the post can delete"
+      end
+      redirect_to posts_path
+    end
+
   def new
     @post = Post.new
   end
+
   def create
-    @post = Post.create!(post_params)
+    @post = Post.create!(post_params.merge(user: current_user))
     redirect_to post_path(@post)
   end
+
+  def create
+  @post = current_user.posts.create!(post_params)
+  redirect_to post_path(@post)
+  end
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
